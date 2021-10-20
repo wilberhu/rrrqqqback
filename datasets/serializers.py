fc0813f3861dc4d41483a90617cf6e7389e51e2d
wilber <wilberhutx@gmail.com>
2021-10-20 16:41:57 +0800

    update

--- datasets/serializers.py
+++ datasets/serializers.py
@@ -13,7 +13,7 @@ class DatasetSerializer(serializers.HyperlinkedModelSerializer):
 
     def to_representation(self, instance):
         representation = super(DatasetSerializer, self).to_representation(instance)
-        representation['path'] = representation['file'].lstrip(os.path.join(MEDIA_URL.strip('/'), strategy_path, representation['owner']) + '/')
+        representation['path'] = representation['file'].split(os.path.join(MEDIA_URL.strip('/'), strategy_path, representation['owner']) + '/', 2)[1]
         if instance.file.size < 1024:
             representation['size'] = str(instance.file.size) + "B"
         elif 1024 <= instance.file.size < 1024*1024:
